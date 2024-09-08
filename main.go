ebe946a5063b689714943403a72326d40cce975c
Exc0mmun1cad0 <fauxmen4@gmail.com>
2024-09-08 23:50:02 +0300

    cleaned main

--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"context"
-	"test-app/cache"
-)
-
 func main() {
-	ctx := context.Background()
 
-	cache, _ := cache.NewRedisCache(ctx, "localhost:6379", "", 0)
-
-	_ = cache
 }
