2a94011e7ffe88e9ccbb44c569a33f1ea0dd0a90
Exc0mmun1cad0 <fauxmen4@gmail.com>
2024-09-08 23:47:54 +0300

    redis implemented

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"context"
+	"test-app/cache"
+)
 
+func main() {
+	ctx := context.Background()
+
+	cache, _ := cache.NewRedisCache(ctx, "localhost:6379", "", 0)
+
+	_ = cache
 }
