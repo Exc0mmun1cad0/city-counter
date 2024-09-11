07eed06e3d591773afdc1b1089ecb72ebbe9febe
Exc0mmun1cad0 <fauxmen4@gmail.com>
2024-09-12 02:54:53 +0300

    handler is ready

--- container/app.go
+++ container/app.go
@@ -22,32 +22,50 @@ func NewApp(storage storage.Storage, cache cache.Cache) *App {
 }
 
 func (a *App) GetStats(w http.ResponseWriter, r *http.Request) {
+	// Background context for cache queries
 	ctx := context.Background()
 
-	countryCode := r.URL.Path
-
-	// cityCnt, err := a.cache.Get(ctx, countryCode)
-	// if err != nil {
-	// 	log.Println("cannot get value from redis: %s", err)
-	// 	return
-	// }
-	// if cityCnt == "0" {
-	// 	cityCnt, err := a.storage.CountCitiesInCountry(countryCode)
-	// 	if err != nil {
-	// 		log.Println("cannot get value from postgres: %w", err)
-	// 	}
-	// 	fmt.Fprintf(w, "В %s %d городов (из постгреса)", countryCode, cityCnt)
-	// 	a.cache.Insert(ctx, countryCode, strconv.Itoa(cityCnt))
-	// } else {
-	// 	fmt.Fprintf(w, "В %s %s городов (из кэша)", countryCode, cityCnt)
-	// }
-
-	cityCnt, err := a.storage.CountCitiesInCountry(countryCode)
-	if err != nil {
-		log.Println(err)
+	// Getting and validating country code.
+	// If there are any symbols except english capital letteres,
+	// user will get 404.
+	countryCode := r.URL.Path[1:]
+	if !validateCountryCode(countryCode) {
+		w.WriteHeader(404)
+		return
 	}
 
-	fmt.Fprintf(w, "%d", cityCnt)
+	// Getting number of cities in country by country code from cache
+	cnt, err := a.cache.Get(ctx, countryCode)
+	if err != nil {
+		log.Printf("cannot get value from redis: %s", err)
+	}
 
-	_ = ctx
+	// If value is equal to 0, there is no data in cache
+	// So we have to do query to the database and cache that value for the future
+	if cnt == 0 {
+		cnt, err := a.storage.CountCitiesInCountry(countryCode)
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+		if cnt == 0 {
+			w.WriteHeader(404)
+			return
+		}
+		a.cache.Insert(ctx, countryCode, cnt)
+		fmt.Fprintf(w, "%d (from db)", cnt)
+		return
+	}
+
+	fmt.Fprintf(w, "%d (from cache)", cnt)
+}
+
+func validateCountryCode(countryCode string) bool {
+	for _, sym := range countryCode {
+		if !(sym >= 65 && sym <= 90) {
+			return false
+		}
+	}
+
+	return true
 }
